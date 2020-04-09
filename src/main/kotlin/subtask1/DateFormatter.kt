70f83b38c1b7f0cb4a38d0a61ddd25c43084febd
Hg Hg <hghghghg.hh39@gmail.com>
2020-04-09 22:41:10 +0300

    feat: finish 1 & 2 tasks

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,47 @@
 package subtask1
 
+import java.time.DateTimeException
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.Month
+import java.util.*
+
 class DateFormatter {
 
-    // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+        val locale = Locale("ru")
+        Locale.setDefault(locale)
+        val dayNum = day.toInt()
+        val monthNum = month.toInt()
+        val yearNum = year.toInt()
+        var resultString: String
+        try {
+            val date = LocalDate.of(yearNum, monthNum, dayNum)
+            resultString = day
+            resultString += when (date.month){
+                Month.JANUARY -> " января, "
+                Month.FEBRUARY -> " февраля, "
+                Month.MARCH -> " марта, "
+                Month.APRIL -> " апреля, "
+                Month.MAY -> " мая, "
+                Month.JUNE -> " июня, "
+                Month.JULY -> " июля, "
+                Month.AUGUST -> " августа, "
+                Month.SEPTEMBER -> " сентября, "
+                Month.OCTOBER -> " октября, "
+                Month.NOVEMBER -> " ноября, "
+                Month.DECEMBER -> " декабря, "
+            }
+            resultString += when (date.dayOfWeek){
+                DayOfWeek.MONDAY -> "понедельник"
+                DayOfWeek.TUESDAY -> "вторник"
+                DayOfWeek.WEDNESDAY -> "среда"
+                DayOfWeek.THURSDAY -> "четверг"
+                DayOfWeek.FRIDAY -> "пятница"
+                DayOfWeek.SATURDAY -> "суббота"
+                DayOfWeek.SUNDAY -> "воскресенье"
+            }
+        } catch (e: DateTimeException){return "Такого дня не существует"}
+        return resultString
     }
 }
--- src/main/kotlin/subtask2/TimeConverter.kt
+++ src/main/kotlin/subtask2/TimeConverter.kt
@@ -4,6 +4,61 @@ class TimeConverter {
 
     // TODO: Complete the following function
     fun toTextFormat(hour: String, minute: String): String {
-        throw NotImplementedError("Not implemented")
+        var h = hour.toInt()
+        var min = minute.toInt()
+        var resultString = ""
+        if (h in 0..12 && min in 0..60){
+            if (min==0) resultString += digitToText(h) + " o' clock"
+            else if (min == 30) resultString += "half past " + digitToText(h)
+            else if (min == 15) resultString += "quarter past " + digitToText(h)
+            else if (min == 45) resultString += if (h!=12) "quarter to " + digitToText(h+1) else "quater to one"
+            else if (min<30){
+                resultString += digitToText(min) + " minute"
+                if (min!=1) resultString+="s"
+                resultString += " past " + digitToText(h)}
+            else {
+                min=60-min
+                resultString += digitToText(min) + " minute"
+                if (min!=1) resultString+="s"
+                resultString += if (h!=12) " to " +  digitToText(h+1) else " to one"
+            }
+        }
+        return resultString
+    }
+
+    fun digitToText(digit: Int): String{
+        return when (digit){
+            0 -> "zero"
+            1 -> "one"
+            2 -> "two"
+            3 -> "three"
+            4 -> "four"
+            5 -> "five"
+            6 -> "six"
+            7 -> "seven"
+            8 -> "eight"
+            9 -> "nine"
+            10 -> "ten"
+            11 -> "eleven"
+            12 -> "twelve"
+            13 -> "thirteen"
+            14 -> "fourteen"
+            15 -> "fifteen"
+            16 -> "sixteen"
+            17 -> "seventeen"
+            18 -> "eighteen"
+            19 -> "nineteen"
+            20 -> "twenty"
+            21 -> "twenty one"
+            22 -> "twenty two"
+            23 -> "twenty three"
+            24 -> "twenty four"
+            25 -> "twenty five"
+            26 -> "twenty six"
+            27 -> "twenty seven"
+            28 -> "twenty eight"
+            29 -> "twenty nine"
+            else -> ""
+        }
     }
 }
